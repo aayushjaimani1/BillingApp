aa33789e66202f64f19d4b974f220d7924db2345
Amanrana2002 <72211447+Amanrana2002@users.noreply.github.com>
2023-05-13 20:17:14 +0530

    coupon button

--- src/app/dashboard/coupon/coupon.component.ts
+++ src/app/dashboard/coupon/coupon.component.ts
@@ -22,91 +22,15 @@ import { fromEvent } from 'rxjs';
   styleUrls: ['./coupon.component.scss']
 })
 export class CouponComponent implements OnInit {
-  private currBranch = ''
-  private ajax: any
-  products: Product[] = []
-  closeResult = '';
-  private formdata: any;
-  private formdata2: any;
-  private formdata3: any;
-  product = this.fb.group({
-    name: ['', Validators.required],
-    sku: ['', Validators.required],
-    category: ['', Validators.required],
-    stock: ['', Validators.required],
-    price: ['', Validators.required],
-    image: ['', Validators.required]
-  });
-  addstock = this.fb.group({
-    stock_sku: ['', Validators.required],
-    stock_stock: ['', Validators.required]
-  })
 
-  multiupload =  this.fb.group({
-    excelfile: [null]
-  })
-
+  closeResult = ""
 
   constructor(private dService: DashboardService, private modalService: NgbModal, private fb: FormBuilder) {
 
   }
 
   ngOnInit(): void {
-    this.currBranch = this.dService.branch
-    this.ajax = this.dService.getProduct(this.currBranch, this.products.length)
-    this.ajax.subscribe((response: any) => {
-      if (response != 'No product found') {
-        this.products = response
-      } else {
-        this.products = []
-      }
-    }, (error: string) => {
-      console.log(error);
-    })
-    this.dService.onBranchNameStateChange.subscribe(value => {
-      this.currBranch = value
-      this.ajax = this.dService.getProduct(value, 0)
-      this.ajax.subscribe((response: any) => {
-        if (response != 'No product found') {
-          this.products = response
-        } else {
-          this.products = []
-        }
-      }, (error: string) => {
-        console.log(error);
-      })
-    })
 
-    fromEvent(document, "scroll").subscribe((event) => {
-      let pos = document.documentElement.scrollTop + window.innerHeight;
-      let max = document.documentElement.scrollHeight - 5;
-      if (pos >= max) {
-        this.ajax = this.dService.getProduct(this.currBranch, this.products.length)
-        this.ajax.subscribe((response: any) => {
-          if (response != 'No product found') {
-            this.products = this.products.concat(response)
-          } else {
-            this.products = []
-          }
-        }, (error: string) => {
-          console.log(error);
-        })
-      }
-
-    })
-
-  }
-  openModal(content: any) {
-    this.modalService.open(content, {
-      ariaLabelledBy: 'modal-basic-title'
-    }).result.then(
-      (result) => {
-        this.closeResult = `Closed with: ${result}`;
-      },
-      (reason) => {
-        this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-      },
-    );
   }
   private getDismissReason(reason: any): string {
     if (reason === ModalDismissReasons.ESC) {
@@ -117,98 +41,6 @@ export class CouponComponent implements OnInit {
       return `with: ${reason}`;
     }
   }
-
-  AddProduct(modal: any) {
-    if (this.product.valid) {
-      this.formdata = new FormData();
-      for (const [key, value] of Object.entries(this.product.value)) {
-        this.formdata.append(key, value)
-      }
-      this.formdata.append('branch', this.currBranch.replace(/[^a-zA-Z0-9\s]/g, ''))
-      this.dService.addSingleProduct(this.formdata).subscribe((response: any) => {
-          if (response == "success") {
-            this.ajax = this.dService.getProduct(this.currBranch, 0)
-            this.ajax.subscribe((response: any) => {
-              if (response != 'No product found') {
-                this.products = response
-              } else {
-                this.products = []
-              }
-            }, (error: string) => {
-              console.log(error);
-            })
-            modal.close()
-          } else {
-            alert(response.error);
-          }
-
-        },
-        (error: any) => {
-          console.log(error);
-        }
-      )
-    } else {
-      alert("Some fields are empty");
-    }
-  }
-
-  delete(sku: string){
-    this.formdata3 = new FormData();
-    this.formdata3.append('delete_sku',sku)
-    this.formdata3.append('branch', this.currBranch.replace(/[^a-zA-Z0-9\s]/g, ''))
-    this.dService.deleteProduct(this.formdata3).subscribe((response) => {
-      if(response == "success"){
-        this.ajax = this.dService.getProduct(this.currBranch, 0)
-        this.ajax.subscribe((response: any) => {
-          if (response != 'No product found') {
-            this.products = response
-          } else {
-            this.products = []
-          }
-        }, (error: string) => {
-          console.log(error);
-        })
-      }
-    },(error)=>{
-      console.log(error);
-    })
-  }
-
-  onFileSelected(event: any) {
-    const file = event.target.files[0];
-    
-    const reader = new FileReader();
-    reader.onload = () => {
-      const blob = new Blob([reader.result as ArrayBuffer], { type: file.type });
-      this.formdata2 = new FormData();
-      this.formdata2.append('excel_file', blob, file.name);
-    }
-    reader.readAsArrayBuffer(file);
-  }
-
-  AddMultiProduct(modal: any){
-    if (this.multiupload.valid) {
-        this.formdata2.append('branch', this.currBranch.replace(/[^a-zA-Z0-9\s]/g, ''))
-        this.dService.addMultipleProduct(this.formdata2).subscribe((response)=>{
-          if(response == "Success"){
-            this.ajax = this.dService.getProduct(this.currBranch, 0)
-            this.ajax.subscribe((response: any) => {
-              if (response != 'No product found') {
-                this.products = response
-              } else {
-                this.products = []
-              }
-            }, (error: string) => {
-              console.log(error);
-            })
-            modal.close()
-          }
-        })
-    } else {
-      alert("Some fields are empty");
-    }
-  }
-
   stockModal(content: any) {
     this.modalService.open(content, {
       ariaLabelledBy: 'modal-basic-title'
@@ -220,41 +52,6 @@ export class CouponComponent implements OnInit {
         this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
       },
     );
-  }
-
-  AddStock(modal: any){
-    if(this.addstock.valid){
-      this.formdata = new FormData()
-      for(const [key, value] of Object.entries(this.addstock.value)){
-        this.formdata.append(key, value)
-      }
-      this.formdata.append('branch', this.currBranch.replace(/[^a-zA-Z0-9\s]/g, ''))
-      this.dService.addProductStock(this.formdata).subscribe((response: any) => {
-        if(response == "success"){
-          this.ajax = this.dService.getProduct(this.currBranch, 0)
-            this.ajax.subscribe((response: any) => {
-              if (response != 'No product found') {
-                this.products = response
-              } else {
-                this.products = []
-              }
-            }, (error: string) => {
-              console.log(error);
-            })
-            modal.close()
-        }
-        else{
-          alert(response);
-        }
-      },
-      (error:any)=>{
-        console.log(error);
-        
-      })
-    }
-    else{
-      alert("Some fields are empty");
-    }
   }
 
 }
